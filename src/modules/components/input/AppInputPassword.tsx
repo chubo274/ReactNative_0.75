5077b0b2362245a7515d6ad8bc7ca8a21d0be469
khanwilson <khanwilson.dev@gmail.com>
2024-12-20 10:29:28 +0700

    Update AppInputPassword.tsx

--- src/modules/components/input/AppInputPassword.tsx
+++ src/modules/components/input/AppInputPassword.tsx
@@ -1,23 +1,24 @@
+import { FastFieldProps } from 'formik';
 import React, { useCallback, useMemo, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import ImageSource from 'src/assets/images';
+import { TxtTypo } from 'src/shared/helpers/enum';
+import { parseStatusFormik } from 'src/shared/helpers/function';
 import { ITheme, useAppTheme } from 'src/shared/theme';
 import { RenderImage } from '../image/RenderImage';
-import { AppInput, IAppInput } from './AppInput';
 import { AppText } from '../text/AppText';
-import { TxtTypo } from 'src/shared/helpers/enum';
+import { AppInput, IAppInput } from './AppInput';
 
-interface IProps extends IAppInput {
-  value?: string
+interface IProps extends Omit<IAppInput, 'value'>, Omit<FastFieldProps, 'meta'> {
 }
 
 export const AppInputPassword = React.memo((props: IProps) => {
-  const { value, status, errorMessage } = props;
+  const { field, form, status, ...rest } = props;
   const theme = useAppTheme();
   const styles = useStyles(theme);
   const [isSecure, setIsSecure] = useState(true)
 
-  const _isSecureMode = useMemo(() => Boolean(isSecure && value), [isSecure, value])
+  const _isSecureMode = useMemo(() => Boolean(isSecure && field.value), [isSecure, field.value])
 
   const widthIcon = theme.dimensions.makeResponsiveSize(24)
   const iconRight = useCallback(() => {
@@ -27,21 +28,30 @@ export const AppInputPassword = React.memo((props: IProps) => {
   }, [isSecure, widthIcon])
 
   const renderErrorMessage = useCallback(() => {
-    if (status == 'error' && errorMessage)
+    const fieldErr = props.form.errors[props.field.name] as string
+    if (status == 'error' && fieldErr)
       return <AppText typo={TxtTypo.Smallest_R} style={{ paddingTop: 4, color: 'red' }}>
-        {errorMessage}
+        {fieldErr}
       </AppText>
-  }, [status, errorMessage])
+  }, [status, props.form.errors, props.field.name])
 
   return <View>
     <View>
       <AppInput
-        {...props}
-        errorMessage={''}
+        value={field.value}
+        onChangeText={form.handleChange(field.name)}
+        status={parseStatusFormik(field.value, form.errors[field.name], form.touched[field.name])}
+        errorMessage={form.errors[field.name] as string}
+        onFocus={() => {
+          form.setFieldTouched(field.name)
+        }}
+        onClear={() => {
+          form.setFieldValue(field.name, undefined)
+        }}
         iconRight={iconRight}
-        value={value}
         secureTextEntry={isSecure}
         inputStyle={[_isSecureMode && { color: 'transparent', fontSize: 10, }]}
+        {...rest}
       />
       {_isSecureMode && <View
         style={styles.viewSecureText}
@@ -50,9 +60,9 @@ export const AppInputPassword = React.memo((props: IProps) => {
           <Text
             numberOfLines={1}
             ellipsizeMode={'clip'}
-            style={{color: theme.color.textColor.primary}}
+            style={{ color: theme.color.textColor.primary }}
           >
-            {value?.replace(/./g, '*')}
+            {field.value?.replace(/./g, '*')}
           </Text>
         </View>
         <View style={{ opacity: 0 }}>
