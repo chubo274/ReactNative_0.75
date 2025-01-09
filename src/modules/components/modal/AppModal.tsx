9d21c3f4cc14b27676b758ab3c6a7ca7e39b1574
khanwilson <khanwilson.dev@gmail.com>
2025-01-09 11:30:09 +0700

    Update AppModal.tsx

--- src/modules/components/modal/AppModal.tsx
+++ src/modules/components/modal/AppModal.tsx
@@ -4,14 +4,15 @@ import React, { useCallback, useRef } from 'react';
 import { TouchableWithoutFeedback, View } from 'react-native';
 import { SharedValue } from 'react-native-reanimated';
 
-interface IProps extends BottomSheetProps {
+interface IProps extends Omit<BottomSheetProps, 'onClose'> {
   snapPoints: Array<string | number> | SharedValue<Array<string | number>>
   backdropOpacity?: number
   onOpen?: () => void
+  onClose?: () => void
 }
 
-export const AppModal = React.memo(React.forwardRef((props: IProps, ref: React.ForwardedRef<BottomSheet | null>) => {
-  const { backdropOpacity, onOpen, onChange, ...rest } = props;
+export const AppBottomSheet = React.memo(React.forwardRef((props: IProps, ref: React.ForwardedRef<BottomSheet | null>) => {
+  const { backdropOpacity, onOpen, onClose, onChange, ...rest } = props;
   const currentSnapIndex = useRef<number>(-1); // Track current index
 
   const close = useCallback(() => {
@@ -21,14 +22,19 @@ export const AppModal = React.memo(React.forwardRef((props: IProps, ref: React.F
 
   const onChangeSnapPoint = useCallback((index: number, position: number, type: SNAP_POINT_TYPE) => {
     const hasOpened = currentSnapIndex.current === -1 && index >= 0;
+    const hasClosed = currentSnapIndex.current >= 0 && index === -1;
     // const hasClosed = index === -1;
 
     if (hasOpened) {
       onOpen?.()
     }
+
+    if (hasClosed) {
+      onClose?.()
+    }
     onChange?.(index, position, type)
     currentSnapIndex.current = index
-  }, [currentSnapIndex, onChange])
+  }, [currentSnapIndex, onOpen, onClose, onChange])
 
   const renderBackdrop = useCallback((props: BottomSheetBackdropProps) => (
     <BottomSheetBackdrop
