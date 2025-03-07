fe6541e78577bdce718b353b2b60284cbf74343d
khanwilson <khanwilson.dev@gmail.com>
2025-03-07 13:02:24 +0700

    Update dimensions.ts

--- src/shared/theme/dimensions.ts
+++ src/shared/theme/dimensions.ts
@@ -1,11 +1,12 @@
 import { PixelRatio, Dimensions, Platform, StatusBar } from 'react-native'
+import { initialWindowMetrics } from 'react-native-safe-area-context';
 
 const width = Dimensions.get('window').width
-const height = Dimensions.get('window').height
-const screenHeight = Dimensions.get('screen').height
+const height = Dimensions.get('window').height // height without Status and Bar
+const screenHeight = Dimensions.get('screen').height // height include Status and Bar
 const navbarHeight = screenHeight - height + (StatusBar.currentHeight ?? 0)
 
-const scale = width / 375
+const scale = width / 430
 
 const responsiveSize = (size: number) => {
   const newSize = size * scale
@@ -16,13 +17,13 @@ export const isIphoneX = () => {
   const dimen = Dimensions.get('window')
   return (
     Platform.OS === 'ios' &&
-        !Platform.isPad &&
-        !Platform.isTV &&
-        ((dimen.height === 780 || dimen.width === 780) ||
-            (dimen.height === 812 || dimen.width === 812) ||
-            (dimen.height === 844 || dimen.width === 844) ||
-            (dimen.height === 896 || dimen.width === 896) ||
-            (dimen.height === 926 || dimen.width === 926))
+    !Platform.isPad &&
+    !Platform.isTV &&
+    ((dimen.height === 780 || dimen.width === 780) ||
+      (dimen.height === 812 || dimen.width === 812) ||
+      (dimen.height === 844 || dimen.width === 844) ||
+      (dimen.height === 896 || dimen.width === 896) ||
+      (dimen.height === 926 || dimen.width === 926))
   );
 }
 
@@ -30,10 +31,10 @@ export const isIphoneDynamicIsland = () => {
   const dimen = Dimensions.get('window')
   return (
     Platform.OS === 'ios' &&
-        !Platform.isPad &&
-        !Platform.isTV &&
-        ((dimen.height === 852 || dimen.width === 852) ||
-            (dimen.height === 932 || dimen.width === 932))
+    !Platform.isPad &&
+    !Platform.isTV &&
+    ((dimen.height === 852 || dimen.width === 852) ||
+      (dimen.height === 932 || dimen.width === 932))
   );
 }
 
@@ -43,14 +44,18 @@ const statusBarHeight = Platform.select({
   default: 0
 })
 
+const heightHeader = Platform.OS == 'ios' ? (isIphoneDynamicIsland() ? 120 : (isIphoneX() ? 100 : 80)) : 100
+
 const dimensions = {
   androidBottomNavHeight: Platform.OS === 'android' ? navbarHeight : 0,
-  deviceHeight: height,
+  deviceHeight: screenHeight,
   deviceWidth: width,
   makeResponsiveSize: responsiveSize,
   getStatusBarHeight: statusBarHeight,
-  getBottomSpacing: responsiveSize(40),
-  getTabBottomHeight: responsiveSize(86),
+  getHeightHeader: heightHeader,
+  getBottomSpacing: (initialWindowMetrics?.insets?.bottom || responsiveSize(16)) + responsiveSize(16),
+  getTabBottomHeight: responsiveSize(80),
+  fullHeightSafe: screenHeight - (initialWindowMetrics?.insets?.bottom || 0) - (Platform.OS == 'android' ? (StatusBar.currentHeight || 0) : 0),
   p2: responsiveSize(2),
   p4: responsiveSize(4),
   p6: responsiveSize(6),
@@ -59,6 +64,7 @@ const dimensions = {
   p12: responsiveSize(12),
   p14: responsiveSize(14),
   p16: responsiveSize(16),
+  p18: responsiveSize(18),
   p20: responsiveSize(20),
   p24: responsiveSize(24),
   p28: responsiveSize(28),
@@ -73,23 +79,16 @@ const dimensions = {
 
 const fontSize = {
   makeResponsiveSize: responsiveSize,
-  p2: responsiveSize(2),
-  p4: responsiveSize(4),
-  p6: responsiveSize(6),
-  p8: responsiveSize(8),
-  p11: responsiveSize(11),
   p12: responsiveSize(12),
-  p13: responsiveSize(13),
-  p15: responsiveSize(15),
+  p14: responsiveSize(14),
   p16: responsiveSize(16),
-  p18: responsiveSize(18),
   p20: responsiveSize(20),
   p24: responsiveSize(24),
-  p28: responsiveSize(28),
   p32: responsiveSize(32),
-  p36: responsiveSize(36),
-  p40: responsiveSize(40),
-  p48: responsiveSize(48),
+  p42: responsiveSize(42),
+  p52: responsiveSize(52),
+  p72: responsiveSize(72),
+  p120: responsiveSize(120),
 }
 
 export default { dimensions, fontSize }
