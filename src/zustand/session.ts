93bbc2ef1a575e86680fd040a57c80844622b124
khanwilson <khanwilson.dev@gmail.com>
2025-03-18 14:28:37 +0700

    Update session.ts

--- src/zustand/session.ts
+++ src/zustand/session.ts
@@ -1,32 +1,48 @@
 import { create, StoreApi, UseBoundStore } from 'zustand';
 import { ZustandSessionModel } from './IZustandSessionModel';
 
-// config store
-type UseSave = <K extends keyof ZustandSessionModel, V extends ZustandSessionModel[K]>(key: K, value: V) => void;
+// Config store
+type UseSave = <K extends keyof ZustandSessionModel, V extends ZustandSessionModel[K]>(key: K, value: V, mode?: 'update') => void;
 type UseGet = <K extends keyof ZustandSessionModel>(key: K) => ZustandSessionModel[K];
 
 interface IRootState {
-	state: ZustandSessionModel;
-	save: UseSave;
-	get: UseGet;
+  state: ZustandSessionModel;
+  save: UseSave;
+  get: UseGet;
 }
 
 const ZustandSession: UseBoundStore<StoreApi<IRootState>> = create((set, get) => ({
   state: {},
-  save: (key, value) => {
-    return set((rootState: any) => {
+  save: (key, value, mode) => {
+    return set((rootState) => {
+      const prevState = rootState.state[key];
+      if (mode && Boolean(prevState)) {
+
+        if (typeof prevState === 'object' && !Array.isArray(prevState)) {
+          return {
+            state: {
+              ...rootState.state,
+              // @ts-ignore
+              [key]: { ...prevState, ...value },
+            },
+          };
+        }
+
+        console.error(`typeof ${key} may not be an object or undefined`);
+        return rootState;
+      }
+
       return {
         state: {
-          ...rootState?.state,
+          ...rootState.state,
           [key]: value,
         },
       };
     });
   },
-  get: (key) => get()?.state?.[key],
-}),
-);
+  get: (key) => get().state[key],
+}));
 
-export const useSaveSession = () => ZustandSession((rootState) => rootState?.save);
-export const useGetSession = <K extends keyof ZustandSessionModel>(key: K) => ZustandSession((rootState) => rootState?.state?.[key]);
-export default ZustandSession
+export const useSaveSession = () => ZustandSession((rootState) => rootState.save);
+export const useGetSession = <K extends keyof ZustandSessionModel>(key: K) => ZustandSession((rootState) => rootState.state[key]);
+export default ZustandSession;
