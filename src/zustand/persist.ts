fab3e8d53e49c615927e28abe606e74d5ae66ce0
khanwilson <khanwilson.dev@gmail.com>
2025-03-18 14:28:54 +0700

    Update persist.ts

--- src/zustand/persist.ts
+++ src/zustand/persist.ts
@@ -4,33 +4,53 @@ import { createJSONStorage, persist } from 'zustand/middleware';
 import { ZustandPersistModel } from './IZustandPersistModel';
 
 // config store
-type UseSave = <K extends keyof ZustandPersistModel, V extends ZustandPersistModel[K]>(key: K, value: V) => void;
+type UseSave = <K extends keyof ZustandPersistModel, V extends ZustandPersistModel[K]>(key: K, value: V, mode?: 'update') => void;
 type UseGet = <K extends keyof ZustandPersistModel>(key: K) => ZustandPersistModel[K];
 type UseClear = <K extends keyof ZustandPersistModel>(key: K) => void;
 
 interface IRootState {
-    state: ZustandPersistModel;
-    save: UseSave;
-    get: UseGet;
-    clear: UseClear;
+  state: ZustandPersistModel;
+  save: UseSave;
+  get: UseGet;
+  clear: UseClear;
 }
 
 const ZustandPersist = create<IRootState>()(
   persist(
     (set, get) => ({
       state: {},
-      save: (key, value) => set({
-        state: { ...get().state, [key]: value }
-      }),
+      save: (key, value, mode) => {
+        const rootState = get().state;
+        const prevState = rootState[key];
+        if (mode && Boolean(prevState)) {
+
+          if (typeof prevState === 'object' && !Array.isArray(prevState)) {
+            return set({
+              state: {
+                ...rootState,
+                // @ts-ignore
+                [key]: { ...prevState, ...value },
+              },
+            });
+          }
+
+          console.error(`typeof ${key} maybe not is object or undefined`);
+          return rootState;
+        }
+
+        return set({
+          state: { ...rootState, [key]: value },
+        });
+      },
       get: (key) => get().state[key],
       clear: (key) => {
-        set({
-          state: { ...get().state, [key]: undefined }
-        })
+        const rootState = { ...get().state };
+        delete rootState[key]; // 🔥 Xóa đúng key thay vì set `undefined`
+        set({ state: rootState });
       },
     }),
     {
-      name: 'Zustand-Persist', // The key used for storage
+      name: 'Zustand-Persist',
       storage: createJSONStorage(() => AsyncStorage),
     }
   )
