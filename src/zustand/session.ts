0e78fa6f48819f253528592390182336d1ee1627
khanwilson <khanwilson.dev@gmail.com>
2025-03-21 15:47:16 +0700

    Update session.ts

--- src/zustand/session.ts
+++ src/zustand/session.ts
@@ -1,48 +1,35 @@
-import { create, StoreApi, UseBoundStore } from 'zustand';
+import { create } from 'zustand';
 import { ZustandSessionModel } from './IZustandSessionModel';
 
 // Config store
-type UseSave = <K extends keyof ZustandSessionModel, V extends ZustandSessionModel[K]>(key: K, value: V, mode?: 'update') => void;
-type UseGet = <K extends keyof ZustandSessionModel>(key: K) => ZustandSessionModel[K];
-
-interface IRootState {
-  state: ZustandSessionModel;
-  save: UseSave;
-  get: UseGet;
+interface IRootState extends ZustandSessionModel {
+  save<K extends keyof ZustandSessionModel, V extends ZustandSessionModel[K]>(key: K, value: V, mode?: 'update'): void;
+  hydrated: boolean | undefined
+  changeHydrated(zustandReady: boolean): void
 }
 
-const ZustandSession: UseBoundStore<StoreApi<IRootState>> = create((set, get) => ({
-  state: {},
-  save: (key, value, mode) => {
-    return set((rootState) => {
-      const prevState = rootState.state[key];
+const ZustandSession = create<IRootState>()(
+  (set, get) => ({
+    hydrated: undefined,
+    save: (key, value, mode) => {
+      const prevState = get()?.[key];
       if (mode && Boolean(prevState)) {
 
         if (typeof prevState === 'object' && !Array.isArray(prevState)) {
-          return {
-            state: {
-              ...rootState.state,
-              // @ts-ignore
-              [key]: { ...prevState, ...value },
-            },
-          };
+          return set({
+            // @ts-ignore
+            [key]: { ...prevState, ...value },
+          });
         }
 
-        console.error(`typeof ${key} may not be an object or undefined`);
-        return rootState;
+        console.error(`typeof ${key} maybe not is object or undefined`);
+        return;
       }
 
-      return {
-        state: {
-          ...rootState.state,
-          [key]: value,
-        },
-      };
-    });
-  },
-  get: (key) => get().state[key],
-}));
+      return set({ [key]: value });
+    },
+    changeHydrated: (nextState: boolean) => set({ hydrated: nextState })
+  }),
+);
 
-export const useSaveSession = () => ZustandSession((rootState) => rootState.save);
-export const useGetSession = <K extends keyof ZustandSessionModel>(key: K) => ZustandSession((rootState) => rootState.state[key]);
 export default ZustandSession;
